b141bd6a27b0277a42db2965dada64bc83d53f46
zanllp <qc@zanllp.cn>
2023-03-26 17:20:57 +0800

    fix

--- javascript/index.js
+++ javascript/index.js
@@ -1,4 +1,4 @@
-(() => {
+;(() => {
   const html = `<!DOCTYPE html>
 <html lang="en">
   <head>
@@ -16,31 +16,38 @@
 </html>
 `
   const asyncCheck = async (getter, checkSize = 100, timeout = 1000) => {
-    return new Promise(x => {
+    return new Promise((x) => {
       const check = (num = 0) => {
-        const target = getter();
+        const target = getter()
         if (target !== undefined && target !== null) {
           x(target)
-        } else if (num > timeout / checkSize) {// 超时
+        } else if (num > timeout / checkSize) {
+          // 超时
           x(target)
         } else {
-          setTimeout(() => check(++num), checkSize);
+          setTimeout(() => check(++num), checkSize)
         }
-      };
-      check();
-    });
+      }
+      check()
+    })
   }
-  // eslint-disable-next-line no-undef
-  asyncCheck(() => gradioApp().querySelector("#baidu_netdisk_container_wrapper"), 500, Infinity).then((el) => {
+  asyncCheck(
+    // eslint-disable-next-line no-undef
+    () => gradioApp().querySelector('#baidu_netdisk_container_wrapper'),
+    500,
+    Infinity
+  ).then((el) => {
     /**
      * @type {HTMLDivElement}
      */
     const wrap = el
-    wrap.childNodes.forEach(v => wrap.removeChild(v))
+    wrap.childNodes.forEach((v) => wrap.removeChild(v))
     const iframe = document.createElement('iframe')
-    iframe.srcdoc = html 
+    iframe.srcdoc = html
     const updateHeight = () => {
-      iframe.style = `width:100%;height:${(window.innerHeight < 1024) ? '100vh' : (window.innerHeight - 128)}px`
+      iframe.style = `width:100%;height:${
+        window.innerHeight < 1024 ? '100vh' : window.innerHeight - 128 + 'px'
+      }`
     }
     updateHeight()
     window.addEventListener('resize', updateHeight)
