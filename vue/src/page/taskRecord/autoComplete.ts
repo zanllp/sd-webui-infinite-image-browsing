cb0cf0b85e9e2dc28e719a8d2222b1bdb1b43bf7
zanllp <qc@zanllp.cn>
2023-04-10 19:48:56 +0800

    修复walk的刷新，简化快速路径名

--- vue/src/page/taskRecord/autoComplete.ts
+++ vue/src/page/taskRecord/autoComplete.ts
@@ -30,17 +30,17 @@ export const getAutoCompletedTagList = async ({
   const exists = await checkPathExists(Object.values(pathMap).filter(v => v))
   type Keys = keyof typeof pathMap
   const cnMap: Record<Keys, string> = {
-    outdir_txt2img_samples: '文生图的输出目录',
-    outdir_img2img_samples: '图生图的输出目录',
+    outdir_txt2img_samples: '文生图',
+    outdir_img2img_samples: '图生图',
     outdir_save: '使用“保存”按钮保存图像的目录',
-    outdir_extras_samples: '附加功能选项卡的输出目录',
-    additional_networks_extra_lora_path: '扫描 LoRA 模型的附加目录',
-    outdir_grids: '宫格图的输出目录',
-    outdir_img2img_grids: '图生图网格文件夹',
-    outdir_samples: '图像的输出目录',
-    outdir_txt2img_grids: '文生图宫格的输出目录',
-    hypernetworks: '超网络模型的路径',
-    embeddings: 'Embedding的文件夹',
+    outdir_extras_samples: '附加',
+    additional_networks_extra_lora_path: 'LoRA 模型',
+    outdir_grids: '宫格图',
+    outdir_img2img_grids: '图生图网格',
+    outdir_samples: '图像',
+    outdir_txt2img_grids: '文生图宫格',
+    hypernetworks: '超网络模型',
+    embeddings: 'Embedding',
     cwd: '工作文件夹',
     home: 'home'
   }
