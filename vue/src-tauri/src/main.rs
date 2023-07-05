38aff073dd73f0bac5f511b5fea33641cfb6d44c
zanllp <qc@zanllp.cn>
2023-07-05 14:36:43 +0800

    Add a GitHub Action to build the Python backend binary.

--- vue/src-tauri/src/main.rs
+++ vue/src-tauri/src/main.rs
@@ -1,11 +1,8 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use std::net::TcpListener;
-use std::process::Command as StdCommand;
-use std::sync::Mutex;
 use tauri::api::process::Command;
-use tauri::{Manager, RunEvent, WindowEvent};
+use tauri::WindowEvent;
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
