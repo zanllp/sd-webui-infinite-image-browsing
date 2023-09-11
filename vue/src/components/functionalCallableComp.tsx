6e08d64d0e965189933a79b7f24fe3cd4e1d7c18
zanllp <qc@zanllp.cn>
2023-09-12 05:00:48 +0800

    Refactor interaction for improved user experience and fix download bugs

--- vue/src/components/functionalCallableComp.tsx
+++ vue/src/components/functionalCallableComp.tsx
@@ -1,9 +1,11 @@
-import { Input, Modal } from 'ant-design-vue'
+import { Button, Input, Modal } from 'ant-design-vue'
 import { ref } from 'vue'
 import * as Path from '@/util/path'
 import { FileNodeInfo, mkdirs } from '@/api/files'
 import { t } from '@/i18n'
-import { toRawFileUrl } from '@/util'
+import { downloadFiles, toRawFileUrl } from '@/util'
+import { DownloadOutlined } from '@/icon'
+import { isStandalone } from '@/util/env'
 
 export const openCreateFlodersModal = (base: string) => {
   const floderName = ref('')
@@ -35,20 +37,31 @@ export const MultiSelectTips = () => (
   </p>
 )
 
-
-
 export const openVideoModal = (file: FileNodeInfo) => {
   Modal.confirm({
     width: '80vw',
     title: file.name,
     icon: null,
-    content: () => <div style={{
-      display: 'flex',
-      alignItems: 'center',
-      justifyContent: 'center'
-    }}>
-      <video style={{ maxHeight: '80vh' }} src={toRawFileUrl(file)} controls autoplay></video>
-    </div>,
+    content: () => (
+      <div
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          flexDirection: 'column'
+        }}
+      >
+        <video style={{ maxHeight: isStandalone ? '80vh' : '60vh' }} src={toRawFileUrl(file)} controls autoplay></video>
+        <div class="actions" style={{ marginTop: '16px' }}>
+          <Button onClick={() => downloadFiles([toRawFileUrl(file, true)])}>
+            {{
+              icon: <DownloadOutlined/>,
+              default: t('download')
+            }}
+          </Button>
+        </div>
+      </div>
+    ),
     maskClosable: true,
     wrapClassName: 'hidden-antd-btns-modal'
   })
