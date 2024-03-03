9dc1c0dd05302765842ced003ddd1548b673ee36
zanllp <qc@zanllp.cn>
2024-03-03 21:39:16 +0800

    release 0.8.0

--- vue/dist/assets/ImgSliPagePane-204383ae.js
+++
@@ -1,1 +0,0 @@
-import{d as a,o as t,k as s,c as n,c1 as _,q as o}from"./index-c9ff604b.js";const c={class:"img-sli-container"},i=a({__name:"ImgSliPagePane",props:{paneIdx:{},tabIdx:{},left:{},right:{}},setup(l){return(e,r)=>(t(),s("div",c,[n(_,{left:e.left,right:e.right},null,8,["left","right"])]))}});const d=o(i,[["__scopeId","data-v-ae3fb9a8"]]);export{d as default};
