b6c03bd819c235c3083cda76648a7de970e83035
zanllp <qc@zanllp.cn>
2024-03-21 10:58:34 +0800

    fix

--- scripts/iib/tool.py
+++ scripts/iib/tool.py
@@ -529,7 +529,7 @@ def open_file_with_default_app(file_path):
     if system == 'Darwin':  # macOS
         subprocess.call(['open', file_path])
     elif system == 'Windows':  # Windows
-        subprocess.call(['start', file_path], shell=True)
+        subprocess.call(file_path, shell=True)
     elif system == 'Linux':  # Linux
         subprocess.call(['xdg-open', file_path])
     else:
