0184e7b1a622caf5c15df5e39b6c9eb25edc1e2b
zanllp <qc@zanllp.cn>
2025-11-10 22:08:24 +0800

    support webm

--- javascript/index.js
+++ javascript/index.js
@@ -13,7 +13,7 @@ Promise.resolve().then(async () => {
     <link rel="icon" href="/favicon.ico" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Infinite Image Browsing</title>
-    <script type="module" crossorigin src="/infinite_image_browsing/fe-static/assets/index-b50c8b7b.js"></script>
+    <script type="module" crossorigin src="/infinite_image_browsing/fe-static/assets/index-a7581aec.js"></script>
     <link rel="stylesheet" href="/infinite_image_browsing/fe-static/assets/index-a44325c7.css">
   </head>
 
